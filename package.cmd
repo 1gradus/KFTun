163f4026bf67635b0680ef583d03c59a0c8b6c88
1gradus <->
2026-04-15 05:36:31 +0500

    Merge package.cmd and package-win7.cmd

--- package.cmd
+++ package.cmd
@@ -1,36 +1,52 @@
+::
+:: package.cmd [-win7] [--run|--run-dev]
+::
 @echo off
 @setlocal
 @set ERRORLEVEL=
 @cd /D %~dp0
 
-cargo build --release --offline
+if "%1x" == "-win7x" (
+    set PackageName=KFTun-win7
+    set BuildOpts=--target x86_64-win7-windows-msvc -Zunstable-options -Zbuild-std
+    set TargetDir=target\x86_64-win7-windows-msvc\release
+    set RunDevOpts=-win7
+    set RUSTFLAGS=
+    shift /1
+) else (
+    set PackageName=KFTun
+    set BuildOpts=
+    set TargetDir=target\release
+    set RunDevOpts=
+)
+set PackageDir=.\package\%PackageName%
+
+cargo build --release --offline %BuildOpts%
 
 if ERRORLEVEL 1 (
     exit /B %ERRORLEVEL%
 )
 
-set PackageDir=.\package\KFTun
-
-rmdir /S /Q package
+rmdir /S /Q %PackageDir%
 mkdir %PackageDir%
 
-copy /Y target\release\*.exe %PackageDir%\
+copy /Y %TargetDir%\*.exe %PackageDir%\
 copy /Y etc\run.cmd %PackageDir%\
 
 pushd package
-    if not exist KFTun.zip (
+    if not exist %PackageName%.zip (
         where /Q 7z.exe
         if not ERRORLEVEL 1 (
-            7z.exe a -tzip -sse -ssp KFTun.zip KFTun
+            7z.exe a -tzip -sse -ssp %PackageName%.zip %PackageName%
         )
     )
-    if not exist KFTun.zip (
+    if not exist %PackageName%.zip (
         where /Q zip.exe
         if not ERRORLEVEL 1 (
-            zip.exe -r KFTun.zip KFTun
+            zip.exe -r %PackageName%.zip %PackageName%
         )
     )
-    if not exist KFTun.zip (
+    if not exist %PackageName%.zip (
         echo "Not creating the archive. No archivers available."
     )
 popd
@@ -40,5 +56,5 @@ if "%1x" == "--runx" (
     call %PackageDir%\run.cmd
 ) else if "%1x" == "--run-devx" (
     echo ----------------------------------------
-    call etc\run-dev.cmd
+    call etc\run-dev.cmd %RunDevOpts%
 )
